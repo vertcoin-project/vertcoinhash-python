9a6e738a3acc6e624e4627575f4eec267ff48d55
vertiond <35286924+vertiond@users.noreply.github.com>
2022-01-15 20:58:37 -0600

    Build all hash modules and run all tests

--- setup.py
+++ setup.py
@@ -1,24 +1,54 @@
 from setuptools import setup, Extension
 
-verthashsources = [
-	'h2.c',
-       'tiny_sha3/sha3.c'
+vertcoinsources = [
+    'scrypt.c',
+    'Lyra2RE.c',
+    'Sponge.c',
+    'Lyra2.c',
+    'sha3/blake.c',
+    'sha3/groestl.c',
+    'sha3/keccak.c',
+    'sha3/cubehash.c',
+    'sha3/bmw.c',
+    'sha3/skein.c',
+    'h2.c',
+    'tiny_sha3/sha3.c'
 ]
 
-verthashincludes = [
-	'.', 
-	'./tiny_sha3'
+vertcoinincludes = [
+    '.',
+    './sha3',
+    './tiny_sha3'
 ]
+
+
+vtc_scrypt_hash_module = Extension('vtc_scrypt_new',
+                                   sources = vertcoinsources + ['scryptmodule.c'],
+                                   extra_compile_args=['-O3', '-msse3'],
+                                   include_dirs=vertcoinincludes)
+
+lyra2re_hash_module = Extension('lyra2re_hash',
+                                sources = vertcoinsources + ['lyra2remodule.c'],
+                                include_dirs=vertcoinincludes)
+
+lyra2re2_hash_module = Extension('lyra2re2_hash',
+                                 sources = vertcoinsources + ['lyra2re2module.c'],
+                                 include_dirs=vertcoinincludes)
+
+lyra2re3_hash_module = Extension('lyra2re3_hash',
+                                 sources = vertcoinsources + ['lyra2re3module.c'],
+                                 include_dirs=vertcoinincludes)
 
 verthash_module = Extension('verthash',
-                            sources=verthashsources+['verthashmodule.c'],
+                            sources = vertcoinsources + ['verthashmodule.c'],
                             extra_compile_args=['-std=c99'],
-                            include_dirs=verthashincludes)
+                            include_dirs=vertcoinincludes)
 
-setup(name = 'verthash',
-      version = '0.0.1',
-      author_email = 'jameslovejoy1@gmail.com',
-      author = 'James Lovejoy',
-      url = 'https://github.com/metalicjames/verthash-pospace',
-      description = 'Python bindings for Verthash proof of work function',
-      ext_modules = [verthash_module])
+
+setup (name = 'vertcoinhash',
+       version = '1.0.0',
+       author_email = 'vertion@protonmail.com',
+       author = 'vertion',
+       url = 'https://github.com/vertcoin-project/vertcoinhash-python',
+       description = 'Bindings for proof of work used by Vertcoin',
+       ext_modules = [verthash_module, lyra2re3_hash_module, lyra2re2_hash_module, lyra2re_hash_module, vtc_scrypt_hash_module])
--- test.py
+++ test.py
@@ -1,5 +1,9 @@
 import sys
 
+import vtc_scrypt_new
+import lyra2re_hash
+import lyra2re2_hash
+import lyra2re3_hash
 import verthash
 
 teststart = '000000203a297b4b7685170d7644b43e5a6056234cc2414edde454a87580e1967d14c1078c13ea916117b0608732f3f65c2e03b81322efc0a62bcee77d8a9371261970a58a5a715da80e031b02560ad8'
@@ -12,9 +16,23 @@ if sys.version_info[0] < 3:
 else:
     testbin = bytes.fromhex(teststart)
 
-hash_bin = verthash.getPoWHash(testbin, datfile)
+
+hash_bin_scryptn = vtc_scrypt_new.getPoWHash(testbin)
+hash_bin_re = lyra2re_hash.getPoWHash(testbin)
+hash_bin_re2 = lyra2re2_hash.getPoWHash(testbin)
+hash_bin_re3 = lyra2re3_hash.getPoWHash(testbin)
+hash_bin_verthash = verthash.getPoWHash(testbin, datfile)
 
 if sys.version_info[0] < 3:
-    print(hash_bin.encode('hex'))
+    print("scrypt-n", hash_bin_scryptn.encode('hex'))
+    print("lyra2re", hash_bin_re.encode('hex'))
+    print("lyra2re2", hash_bin_re2.encode('hex'))
+    print("lyra2re3", hash_bin_re3.encode('hex'))
+    print("verthash", hash_bin_verthash.encode('hex'))
+
 else:
-    print(hash_bin.hex())
+    print("scrypt-n", hash_bin_scryptn.hex())
+    print("lyra2re", hash_bin_re.hex())
+    print("lyra2re2", hash_bin_re2.hex())
+    print("lyra2re3", hash_bin_re3.hex())
+    print("verthash", hash_bin_verthash.hex())
